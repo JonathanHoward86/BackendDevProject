e8c83d28bea1d1382027b065aac13c55db255f16
Jonathan Howard <Jonathanhoward86@gmail.com>
2023-08-19 20:55:40 -0500

    Added Xunit and Moq for unit testing Borked the project in the process Deleted Bin / Obj folders and rebuilt No errors, testing to verify.

--- Authentication.cs
+++ Authentication.cs
@@ -1,15 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Text;
 using MyEcommerceBackend.Models;
 
 namespace MyEcommerceBackend
