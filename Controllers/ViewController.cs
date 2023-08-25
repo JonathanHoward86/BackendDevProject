67fa4b111d7473314fc58356040b9c08fc420aa4
Jonathan Howard <Jonathanhoward86@gmail.com>
2023-08-25 17:27:23 -0500

    OCD and cleaned up the view controller Removed the redundancies

--- Controllers/ViewController.cs
+++ Controllers/ViewController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using MyEcommerceBackend.Models;
 
 public class ViewController : Controller // Controller for handling user-facing views like Register, Login, etc.
 {
@@ -7,12 +6,6 @@ public class ViewController : Controller // Controller for handling user-facing 
     public IActionResult Register()
     {
         return View(); // Returns the Register view.
-    }
-
-    [HttpPost]
-    public IActionResult Register(RegisterModel model)
-    {
-        return View(model); // Returns the Register view along with the model data.
     }
 
     public IActionResult RegisterSuccess()
@@ -26,12 +19,6 @@ public class ViewController : Controller // Controller for handling user-facing 
         return View(); // Returns the Login view.
     }
 
-    [HttpPost]
-    public IActionResult Login(LoginModel model)
-    {
-        return View(model); // Returns the Login view along with the model data.
-    }
-
     public IActionResult LoginSuccess()
     {
         return View(); // Returns the LoginSuccess view.
@@ -43,32 +30,9 @@ public class ViewController : Controller // Controller for handling user-facing 
         return View(); // Returns the ResetPassword view.
     }
 
-    [HttpPost]
-    public IActionResult ResetPassword(ResetPasswordModel model)
-    {
-        return View(model); // Returns the ResetPassword view along with the model data.
-    }
-
-    [HttpGet]
-    public IActionResult ForgotUsername()
-    {
-        return View(); // Returns the ForgotUsername view.
-    }
-
-    [HttpPost]
-    public IActionResult ForgotUsername(ForgotUsernameModel model)
-    {
-        return View(model); // Returns the ForgotUsername view along with the model data.
-    }
-
     public IActionResult ResetPasswordEmailSent()
     {
         return View(); // Returns the ResetPasswordEmailSent view.
-    }
-
-    public IActionResult ForgotUsernameEmailSent()
-    {
-        return View(); // Returns the ForgotUsernameEmailSent view.
     }
 
     [HttpGet]
@@ -77,14 +41,19 @@ public class ViewController : Controller // Controller for handling user-facing 
         return View(); // Returns the ResetPasswordConfirm view.
     }
 
-    [HttpPost]
-    public IActionResult ResetPasswordConfirm(ResetPasswordConfirmModel model)
-    {
-        return View(model); // Returns the ResetPasswordConfirm view.
-    }
-
     public IActionResult ResetPasswordSuccess()
     {
         return View(); // Returns the ResetPasswordSuccess view.
+    }
+
+    [HttpGet]
+    public IActionResult ForgotUsername()
+    {
+        return View(); // Returns the ForgotUsername view.
+    }
+
+    public IActionResult ForgotUsernameEmailSent()
+    {
+        return View(); // Returns the ForgotUsernameEmailSent view.
     }
 }
